bd9cd832008fc1c458dc8d539b6dff9170f26b22
Sarapulov Vasiliy <sarapulov.vas@mail.ru>
2024-07-04 18:29:17 +0300

    code restructuring

--- src/main.c
+++ src/main.c
@@ -1,114 +1,119 @@
 #include "main.h"
 #include <stdio.h>
 
+int readFile(char *path, struct BMPFileHeader fileHeader,
+             struct BMPInfo fileInfo) {
+    FILE *file = fopen(path, "r");
+    if (file == NULL) {
+        fprintf(stderr, "The input file open failed.\n");
+        return 1;
+    }
+
+    fread(&fileHeader, sizeof(fileHeader), 1, file);
+    if (fileHeader.Type[0] != 'B' || fileHeader.Type[1] != 'M') {
+        fprintf(stderr, "The file is not a BMP file.\n");
+        return 1;
+    }
+
+    fread(&fileInfo.Size, sizeof(fileInfo.Size), 1, file);
+    fread(&fileInfo.Width, fileInfo.Size - sizeof(fileInfo.Size), 1, file);
+    fclose(file);
+    return 0;
+}
+
+void printHeader(struct BMPFileHeader fileHeader, struct BMPInfo fileInfo) {
+    Print_Line();
+    printf("| %-25s | %25u |\n", "Size", fileHeader.Size);
+    Print_Line();
+    printf("| %-25s | %12d x %-11d|\n", "Width/Height", fileInfo.Width,
+           fileInfo.Height);
+    Print_Line();
+    printf("| %-25s | %25hu |\n", "Bit Count", fileInfo.BitCount);
+    Print_Line();
+    printf("| %-25s | %25u |\n", "Compression", fileInfo.Compression);
+    Print_Line();
+    printf("| %-25s | %25u |\n", "Size image", fileInfo.SizeImage);
+    Print_Line();
+    printf("| %-25s | %12d x %-11d|\n", "Pels per meter", fileInfo.XPelsPerMetr,
+           fileInfo.YPelsPerMetr);
+    Print_Line();
+    printf("| %-25s | %25u |\n", "Clr used", fileInfo.ClrUsed);
+    Print_Line();
+    printf("| %-25s | %25u |\n", "Clr important", fileInfo.ClrImportant);
+    Print_Line();
+    if (fileInfo.Size == Version_3_Size) {
+        return;
+    }
+
+    printf("| %-25s | %25x |\n", "Red mask", fileInfo.RedMask);
+    Print_Line();
+    printf("| %-25s | %25x |\n", "Green mask", fileInfo.GreenMask);
+    Print_Line();
+    printf("| %-25s | %25x |\n", "Blue mask", fileInfo.BlueMask);
+    Print_Line();
+    printf("| %-25s | %25x |\n", "Alpha mask", fileInfo.AlphaMask);
+    Print_Line();
+    printf("| %-25s | %25x |\n", "CSType", fileInfo.CSType);
+    Print_Line();
+    printf("| %-25s | %25x |\n", "EndPoints ciexyz Red X",
+           fileInfo.EndPoints.ciexyzRed.ciexyzX);
+    Print_Line();
+    printf("| %-25s | %25x |\n", "EndPoints ciexyz Red Y",
+           fileInfo.EndPoints.ciexyzRed.ciexyzY);
+    Print_Line();
+    printf("| %-25s | %25x |\n", "EndPoints ciexyz Red Z",
+           fileInfo.EndPoints.ciexyzRed.ciexyzZ);
+    Print_Line();
+    printf("| %-25s | %25x |\n", "EndPoints ciexyz Green X",
+           fileInfo.EndPoints.ciexyzGreen.ciexyzX);
+    Print_Line();
+    printf("| %-25s | %25x |\n", "EndPoints ciexyz Green Y",
+           fileInfo.EndPoints.ciexyzGreen.ciexyzY);
+    Print_Line();
+    printf("| %-25s | %25x |\n", "EndPoints ciexyz Green Z",
+           fileInfo.EndPoints.ciexyzGreen.ciexyzZ);
+    Print_Line();
+    printf("| %-25s | %25x |\n", "EndPoints ciexyz Blue X",
+           fileInfo.EndPoints.ciexyzBlue.ciexyzX);
+    Print_Line();
+    printf("| %-25s | %25x |\n", "EndPoints ciexyz Blue Y",
+           fileInfo.EndPoints.ciexyzBlue.ciexyzY);
+    Print_Line();
+    printf("| %-25s | %25x |\n", "EndPoints ciexyz Blue Z",
+           fileInfo.EndPoints.ciexyzBlue.ciexyzZ);
+    Print_Line();
+    printf("| %-25s | %25x |\n", "Gamma red", fileInfo.GammaRed);
+    Print_Line();
+    printf("| %-25s | %25x |\n", "Gamma green", fileInfo.GammaGreen);
+    Print_Line();
+    printf("| %-25s | %25x |\n", "Gamma blue", fileInfo.GammaBlue);
+    Print_Line();
+    if (fileInfo.Size == Version_4_Size) {
+        return;
+    }
+
+    printf("| %-25s | %25u |\n", "Intent", fileInfo.Intent);
+    Print_Line();
+    printf("| %-25s | %25u |\n", "Profile data", fileInfo.ProfileData);
+    Print_Line();
+    printf("| %-25s | %25u |\n", "Profile size", fileInfo.ProfileSize);
+    Print_Line();
+    printf("| %-25s | %25u |\n", "Reserved", fileInfo.Reserved);
+    Print_Line();
+}
+
 int main(int argc, char *argv[]) {
-   struct BMPFileHeader fileHeader;
-   struct BMPInfo fileInfo;
-   FILE *file;
-   if (argc != 2) {
-      printf(
-          "Wrong number of the incoming parameters\nPlease, try again or use "
-          "\"--help\" or \"-h\"\n");
-      return 1;
-   }
+    struct BMPFileHeader fileHeader;
+    struct BMPInfo fileInfo;
+    if (argc != 2) {
+        printf("Wrong number of the incoming parameters\n");
+        return 1;
+    }
 
-   if (argv[1] == "--help" || argv[1] == "-h") {
-      printf("Enter the path to the BMP file.\n");
-      return 0;
-   }
+    if (readFile(argv[1], fileHeader, fileInfo) == 1) {
+        return 1;
+    }
 
-   file = fopen(argv[1], "r");
-   if (file == NULL) {
-      fprintf(stderr, "The input file open failed.\n");
-      return 1;
-   }
-
-   fread(&fileHeader, sizeof(fileHeader), 1, file);
-   if (fileHeader.Type[0] != 'B' || fileHeader.Type[1] != 'M') {
-      fprintf(stderr, "The file is not a BMP file.\n");
-      return 1;
-   }
-
-   fread(&fileInfo, sizeof(fileInfo.Size), 1, file);
-   fread(&fileInfo.Width, fileInfo.Size - sizeof(fileInfo.Size), 1, file);
-   fclose(file);
-   PrintLine();
-   printf("| %-25s | %25u |\n", "Size", fileHeader.Size);
-   PrintLine();
-   printf("| %-25s | %12d x %-11d|\n", "Width/Height", fileInfo.Width,
-          fileInfo.Height);
-   PrintLine();
-   printf("| %-25s | %25u |\n", "Bit Count", fileInfo.BitCount);
-   PrintLine();
-   printf("| %-25s | %25u |\n", "Compression", fileInfo.Compression);
-   PrintLine();
-   printf("| %-25s | %25u |\n", "Size image", fileInfo.SizeImage);
-   PrintLine();
-   printf("| %-25s | %12d x %-11d|\n", "Pels per meter", fileInfo.XPelsPerMetr,
-          fileInfo.YPelsPerMetr);
-   PrintLine();
-   printf("| %-25s | %25u |\n", "Clr used", fileInfo.ClrUsed);
-   PrintLine();
-   printf("| %-25s | %25u |\n", "Clr important", fileInfo.ClrImportant);
-   PrintLine();
-   if (fileInfo.Size == Version3Size) {
-      return 0;
-   }
-
-   printf("| %-25s | %25x |\n", "Red mask", fileInfo.RedMask);
-   PrintLine();
-   printf("| %-25s | %25x |\n", "Green mask", fileInfo.GreenMask);
-   PrintLine();
-   printf("| %-25s | %25x |\n", "Blue mask", fileInfo.BlueMask);
-   PrintLine();
-   printf("| %-25s | %25x |\n", "Alpha mask", fileInfo.AlphaMask);
-   PrintLine();
-   printf("| %-25s | %25x |\n", "CSType", fileInfo.CSType);
-   PrintLine();
-   printf("| %-25s | %25x |\n", "EndPoints ciexyz Red X",
-          fileInfo.EndPoints.ciexyzRed.ciexyzX);
-   PrintLine();
-   printf("| %-25s | %25x |\n", "EndPoints ciexyz Red Y",
-          fileInfo.EndPoints.ciexyzRed.ciexyzY);
-   PrintLine();
-   printf("| %-25s | %25x |\n", "EndPoints ciexyz Red Z",
-          fileInfo.EndPoints.ciexyzRed.ciexyzZ);
-   PrintLine();
-   printf("| %-25s | %25x |\n", "EndPoints ciexyz Green X",
-          fileInfo.EndPoints.ciexyzGreen.ciexyzX);
-   PrintLine();
-   printf("| %-25s | %25x |\n", "EndPoints ciexyz Green Y",
-          fileInfo.EndPoints.ciexyzGreen.ciexyzY);
-   PrintLine();
-   printf("| %-25s | %25x |\n", "EndPoints ciexyz Green Z",
-          fileInfo.EndPoints.ciexyzGreen.ciexyzZ);
-   PrintLine();
-   printf("| %-25s | %25x |\n", "EndPoints ciexyz Blue X",
-          fileInfo.EndPoints.ciexyzBlue.ciexyzX);
-   PrintLine();
-   printf("| %-25s | %25x |\n", "EndPoints ciexyz Blue Y",
-          fileInfo.EndPoints.ciexyzBlue.ciexyzY);
-   PrintLine();
-   printf("| %-25s | %25x |\n", "EndPoints ciexyz Blue Z",
-          fileInfo.EndPoints.ciexyzBlue.ciexyzZ);
-   PrintLine();
-   printf("| %-25s | %25x |\n", "Gamma red", fileInfo.GammaRed);
-   PrintLine();
-   printf("| %-25s | %25x |\n", "Gamma green", fileInfo.GammaGreen);
-   PrintLine();
-   printf("| %-25s | %25x |\n", "Gamma blue", fileInfo.GammaBlue);
-   PrintLine();
-   if (fileInfo.Size == Version4Size) {
-      return 0;
-   }
-
-   printf("| %-25s | %25u |\n", "Intent", fileInfo.Intent);
-   PrintLine();
-   printf("| %-25s | %25u |\n", "Profile data", fileInfo.ProfileData);
-   PrintLine();
-   printf("| %-25s | %25u |\n", "Profile size", fileInfo.ProfileSize);
-   PrintLine();
-   printf("| %-25s | %25u |\n", "Reserved", fileInfo.Reserved);
-   PrintLine();
-   return 0;
+    printHeader(fileHeader, fileInfo);
+    return 0;
 }
